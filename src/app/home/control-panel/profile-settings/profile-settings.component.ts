0d5f4faaf987708c9635866150bf68429f0dd168
Nico Ziman <nziman@ausy-group.com>
2020-05-14 15:51:29 +0300

    add profile-settings component

---
+++ src/app/home/control-panel/profile-settings/profile-settings.component.ts
@@ -0,0 +1,31 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Router} from "@angular/router";
+import Routes from '../../../shared/routes/routes';
+
+
+@Component({
+  selector: 'app-profile-settings',
+  templateUrl: './profile-settings.component.html',
+  styleUrls: ['./profile-settings.component.scss']
+})
+export class ProfileSettingsComponent implements OnInit {
+
+  @Output() profileEmitter = new EventEmitter<boolean>();
+  @Input() displayMenu: boolean;
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  logOut() {
+    localStorage.clear();
+    this.router.navigate([Routes.AUTHENTICATION]);
+
+  }
+
+  showProfile() {
+    this.profileEmitter.emit(true);
+  }
+
+}
