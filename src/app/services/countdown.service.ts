59d9231cdc254ffcd4a8f47281772182bc8e1aa7
nicolaeziman <nicolae.ziman@pearson.com>
2020-08-13 18:03:52 +0300

    create a countdown service

---
+++ src/app/services/countdown.service.ts
@@ -0,0 +1,10 @@
+// TODO use this service when it comes to timer calculations
+
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CountdownService {
+
+}
