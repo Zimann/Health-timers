727710c849d3b661f1faaadfd3fa500fb2de2d55
nicolaeziman <nicolae.ziman@pearson.com>
2020-09-09 11:19:43 +0300

    add angular material modules and styling for the checkbox

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatSelectModule } from "@angular/material/select";
 
 
 const MaterialComponents = [
   MatFormFieldModule,
-  MatInputModule
-
+  MatInputModule,
+  MatCheckboxModule,
+  MatSelectModule,
 ];
 
 @NgModule({
