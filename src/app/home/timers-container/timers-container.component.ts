c34927410d4a3b07d707fea6351a48401701cf0e
nicolaeziman <nicolae.ziman@pearson.com>
2020-09-29 18:52:15 +0300

    handle insertion of single timers per column & add stop button

--- src/app/home/timers-container/timers-container.component.ts
+++ src/app/home/timers-container/timers-container.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { Countdown, CountDownTimer, HourConversionUnits, TimerTitle } from "../../shared/models/timer.model";
-import { CrossComponentService } from '../../services/cross-component.service';
+import {Component, OnInit} from '@angular/core';
+import {CountDownTimer, TimerTitle} from "../../shared/models/timer.model";
+import {CrossComponentService} from '../../services/cross-component.service';
 import {CountdownService} from "../../services/countdown.service";
+import {CountdownEvent} from "ngx-countdown";
+import {AudioService} from "../../services/audio.service";
 
 @Component({
   selector: 'app-timers-container',
@@ -13,25 +15,34 @@ export class TimersContainerComponent implements OnInit {
 
   colNames: TimerTitle[] = [
     {timerName: 'Stretching', timerIcon: 'fa-running'},
-    {timerName: 'Fast workout', timerIcon: 'fa-dumbbell'},
+    {timerName: 'Workout', timerIcon: 'fa-dumbbell'},
     {timerName: 'Chilling', timerIcon: 'fa-couch'}
   ];
 
-  // TODO refactor this to fetch the timers from firebase
-  // this will stay like this for now
+  ringTheAlarm: boolean;
 
   countdownTimers: CountDownTimer[] = [];
-  timerName: string;
 
   constructor(private crossComponentService: CrossComponentService,
-              private countDownService: CountdownService) {
+              private countDownService: CountdownService,
+              private audioService: AudioService) {
+  }
+
+  handleCountdownStop(counterData: CountdownEvent) {
+    if (counterData.left === 0) {
+      this.ringTheAlarm = true;
+      this.audioService.playAudio();
+
+    }
   }
 
   ngOnInit(): void {
-    this.crossComponentService.timerData$.subscribe(data => {
-      this.timerName = data.timerType;
-      this.countdownTimers.push({...data, ...this.countDownService.calculateCountDownTime(data)});
+    const timerCategories = {};
+    this.colNames.forEach(el => timerCategories[el.timerName.toLowerCase()]);
+    this.crossComponentService.timerData$.subscribe((data: CountDownTimer) => {
+      timerCategories[data.timerType.toLowerCase()] = {...data, ...this.countDownService.calculateCountDownTime(data)};
+      this.countdownTimers = [];
+      this.countdownTimers = Object.values(timerCategories);
     });
   }
-
 }
