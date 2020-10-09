61e1c24f2b01076eae2f9c2641db770fdf954c3c
nicolaeziman <nicolae.ziman@pearson.com>
2020-10-09 10:10:29 +0300

    remove singleton setup on the audio.service.ts

--- src/app/services/audio.service.ts
+++ src/app/services/audio.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class AudioService {
 
   audioSrc: string = '../../assets/audio/timer-alarm.mp3';
