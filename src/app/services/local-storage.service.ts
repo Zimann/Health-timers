b170670c9b95efbd1119a0a8193dfd697d9f39b0
nicolaeziman <nicolae.ziman@pearson.com>
2020-10-14 18:33:59 +0300

    tidy localStorage service

--- src/app/services/local-storage.service.ts
+++ src/app/services/local-storage.service.ts
@@ -11,28 +11,29 @@ import Routes from "../shared/routes/routes";
 })
 export class LocalStorageService {
 
-  constructor(private router: Router) {
-  }
+  private departureMomentDate: number;
+
+  constructor(private router: Router) { }
 
 
   trackDepartureTime(destroyed$: ReplaySubject<boolean>) {
-
-    let departureMomentDate;
-
     // add local storage timeStamp when navigating away
     window.addEventListener('beforeunload', (e) => {
-      departureMomentDate = Math.round(new Date().getTime() / 1000);
-      localStorage.setItem('departureMomentDate', String(departureMomentDate));
+      this.departureMomentDate = Math.round(new Date().getTime() / 1000);
+      localStorage.setItem('departureMomentDate', String(this.departureMomentDate));
     });
     // route user back to the authentication page once the token expires
-    const tokenExpiryTime = Number(localStorage.getItem('tokenExpiry')) * 1000;
+    let tokenExpiryTime = Number(localStorage.getItem('tokenExpiry')) * 1000;
     const tokenExpiry$ = timer(tokenExpiryTime);
     tokenExpiry$
       .pipe(takeUntil(destroyed$))
       .subscribe(() => {
-        localStorage.clear();
+        this.clearLocalStorage();
         this.router.navigate([Routes.AUTHENTICATION]);
       });
+  }
 
+  clearLocalStorage() {
+    localStorage.clear();
   }
 }
