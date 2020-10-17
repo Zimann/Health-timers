fc6636e0417bf531fd59bdafbe17d7ced38d9b16
nicolaeziman <nicolae.ziman@pearson.com>
2020-10-17 13:29:56 +0300

    build separate form for custom timer

--- src/app/home/control-panel/create-timers/create-custom-timer/create-custom-timer.component.ts
+++ src/app/home/control-panel/create-timers/create-custom-timer/create-custom-timer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-create-custom-timer',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateCustomTimerComponent implements OnInit {
 
-  constructor() { }
+  customTimerForm: FormGroup;
+
+  constructor(private formBuild: FormBuilder,) { }
+
+
 
   ngOnInit(): void {
+    this.customTimerForm = this.formBuild.group({
+      hours: [''],
+      minutes: [''],
+      timerType: ['']
+    });
   }
 
 }
