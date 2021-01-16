9edd7aa5e6481d8fcdffac5cd433272d1fd4396e
nicolaeziman <nicolae.ziman@pearson.com>
2021-01-16 14:26:01 +0200

    remove unnecessary modules

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,15 +1,8 @@
 import { NgModule } from '@angular/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from "@angular/material/checkbox";
-import { MatSelectModule } from "@angular/material/select";
-
 
 const MaterialComponents = [
-  MatFormFieldModule,
-  MatInputModule,
   MatCheckboxModule,
-  MatSelectModule,
 ];
 
 @NgModule({
